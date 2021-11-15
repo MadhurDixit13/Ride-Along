e4dfe9b95140d510ec2e35131f890a42417d5dff
meetgandhii <meetgandhism@gmail.com>
2021-11-15 22:53:39 +0530

    removed bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,15 @@
+// import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Login from "./Login";
+// import "./App.css";
+// import Signup from "./Signup";
+// import Home from "./Home";
+// import { createMuiTheme } from "@material-ui/core/styles";
+// import { ThemeProvider } from "@material-ui/styles";
+// import { AuthProvider } from "./contexts/AuthContext";
+// import PrivateRoute from "./PrivateRoute";
+// import { AppProvider } from "./contexts/AppContext";
+// import Profile from "./Profile";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
